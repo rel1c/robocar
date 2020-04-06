68c3732b3e36dd4a01655624f50f0f99199a9b50
Adam Peterson <mail@adamjp.com>
2020-04-05 19:37:11 -0500

    Added docstrings

--- control/control.py
+++ control/control.py
@@ -7,8 +7,10 @@ from enum import Enum
 # Constants
 ENDIAN = 'little'
 
-# Control commands
 class Command(Enum):
+  """
+  Commands to control the car, represented as an enum of integers.
+  """
   HELLO = 1    # Sent to initialize communications between Arduino and computer.
   OVER = 2     # Sent to signal that a message was received and executed.
   MOTOR = 3    # Sent to signal a change in motor speed.
@@ -17,33 +19,57 @@ class Command(Enum):
   ERROR = 6    # Sent to signal an error.
 
 def read_byte(s):
+  """
+  Read in one byte from serial, and return it as an int.
+  
+  @param s: Serial object to be read.
+  @returns: Byte received from serial.
+  """
   b = s.read(1)
   return int.from_bytes(b, byteorder=ENDIAN, signed=False)
 
 def read_command(s):
+  """
+  Read in one byte from serial, and return it as a Command object.
+  
+  @param s: Serial object to be read.
+  @returns: Command object received from serial.
+  """
   return Command(get_byte(s))
 
 def write_serial(s, msg):
+  """
+  Write a list of bytes to serial.
+  
+  @param s: Serial object to be written.
+  @param msg: List of bytes to be written to serial.
+  """
   m = bytes(msg)
   s.write(m)
 
 def parse_command(c):
+  """
+  Parse a Command into a string.
+  
+  @param c: The Command object to be parsed.
+  @returns: A string representation of a Command.
+  """
   try:
     command = Command(c)
     if command == Command.HELLO:
-      msg = 'HELLO'
+      s = 'HELLO'
     elif command == Command.OVER:
-      msg = 'OVER'
+      s = 'OVER'
     elif command == Command.MOTOR:
-      msg = 'MOTOR'
+      s = 'MOTOR'
     elif command == Command.STEER:
-      msg = 'STEER'
+      s = 'STEER'
     elif command == Command.REVERSE:
-      msg = 'REVERSE'
+      s = 'REVERSE'
     elif command == Command.ERROR:
-      msg = 'ERROR'
+      s = 'ERROR'
     else:
-      msg = ''
+      s = ''
   except Exception as e:
     print('Error in parsing command:', c)
-  return msg
+  return s
