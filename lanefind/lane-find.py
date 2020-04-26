dfde8f67074d1efedbade64fd24309a31b469e3e
Adam Peterson <mail@adamjp.com>
2020-04-25 20:52:05 -0500

    probabilistic hough transform on lower half of image

--- lanefind/lane-find.py
+++ lanefind/lane-find.py
@@ -19,38 +19,39 @@ rawCapture = PiRGBArray(camera, size=(WIDTH, HEIGHT))
 # allow the camera to warmup
 time.sleep(0.1)
 
-# apply Hough transform to processed image, and draw lines on base image
-def find_lines(disp_img, proc_img):
-  lines = cv2.HoughLinesP(proc_img, 1, np.pi/180, 200)
+# apply probabilistic Hough transform to the processed ROI
+def find_lines(roi):
+  return cv2.HoughLinesP(roi, 1, np.pi/180, 200)
 
+# draw lines on image with respect to a specific origin
+def draw_lines(img, lines, x, y):
   for line in lines:
     x1, y1, x2, y2 = line[0]
     #draw lines on image
-    cv2.line(disp_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-  return disp_img
+    cv2.line(img, (x1+x, y1+y), (x2+x, y2+y), (0, 0, 255), 2)
+  return img
+ 
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-  # grab the raw NumPy array representing the image, then initialize the timestamp
-  # and occupied/unoccupied text
-  image = frame.array
+  img = frame.array
 
-  # mask the bottom two thirds of the image,
-  # manipulate just this bottom with the relevant lane markings
-  # BGR -> GRAY -> GaussianBlur -> Contrast -> Threshold
+  # make a ROI for the bottom half of the image
+  roi = img[HEIGHT/2:HEIGHT, 0:WIDTH] 
 
-  #img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #convert image to HSV colorspace
-  #mask = cv2.inRange(img_hsv, (0, 0, )
-
-  img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert the image into black and white colorspace
-  img_blur = cv2.GaussianBlur(img_gray, (9, 9), 0) #blur the image to cut out noise
-  img_cont = cv2.equalizeHist(img_blur) #ehance contrast in image
-  img_thresh = cv2.threshold(img_cont, 200, 255, cv2.THRESH_BINARY)[1]    
-  image = find_lines(image, img_thresh)
+  # process the ROI for line finding
+  roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY) #convert the image into black and white colorspace
+  roi_blur = cv2.GaussianBlur(roi_gray, (9, 9), 0) #blur the image to cut out noise
+  roi_cont = cv2.equalizeHist(roi_blur) #ehance contrast in image
+  roi_thresh = cv2.threshold(roi_cont, 200, 255, cv2.THRESH_BINARY)[1]    
+  
+  # find lines with a Hough transform and draw them on the base image
+  lines = find_lines(roi_thresh)
+  img_line = draw_lines(img, lines, 0, HEIGHT/2)
   
   # show the frame
-  cv2.imshow("Lanes", image)
-  cv2.imshow("Processed", img_thresh)
+  cv2.imshow("Lanes", img_line)
+  cv2.imshow("Processed", roi_thresh)
   key = cv2.waitKey(1) & 0xFF #display the stream for 1ms and capture any key input as a character
 
   # clear the stream in preparation for the next frame
