311f4b1a10d0980f36508096aaf3e414d3917456
Adam Peterson <mail@adamjp.com>
2020-05-08 16:41:14 -0500

    finding angles based off bird's eye projection

--- lanefind/lane_finder.py
+++ lanefind/lane_finder.py
@@ -64,37 +64,64 @@ def isolate_blue(img):
   # return a binary image
   return img_proc
 
-# minimize found lanes to small lines
+# minimize found shapes
 def erode_image(img):
   kernel = np.ones((3,3), np.uint8)
   img_erode = cv2.erode(img, kernel, iterations=5)
   return img_erode
 
-# find lines in image using probabilistic Hough transform
+# apply a probabilistic Hough transform to an image
 def find_lines(img):
-  return cv2.HoughLines(img, 1, np.pi/180, 45)
+  return cv2.HoughLinesP(img, 1, np.pi/180, 55, minLineLength=50, maxLineGap=5)
 
-# draw found line on image
-def draw_line(img, line):
-  color = (255, 200, 100)
-  rho, theta = line
-  a = np.cos(theta)
-  b = np.sin(theta)
-  x0 = a*rho
-  y0 = b*rho
-  x1 = int(x0 + 1000*(-b))
-  y1 = int(y0 + 1000*(a))
-  x2 = int(x0 - 1000*(-b))
-  y2 = int(y0 - 1000*(a))
-  cv2.line(img, (x1, y1), (x2, y2), color, 2)
-  return img
-
-def draw_lines(img, lines):
+# draw found lines on image
+def draw_lines(img, lines, x, y, color=(0, 0, 255)):
   if lines is not None:
-    print len(lines)
-    for line in lines[0]:
-      img = draw_line(img, line)
+    for line in lines:
+      x1, y1, x2, y2 = line[0]
+      cv2.line(img, (x1+x, y1+y), (x2+x, y2+y), color, 2)
   return img
+
+# find the average angle based on the weighted average of found lines
+def average_angle(lines):
+  # by default go forward
+  if lines is None:
+    print 'no lines detected'
+    return 90
+  slopes = []
+  print len(lines)
+  for line in lines:
+    x1, y1, x2, y2 = line[0]
+    if x1 != x2:
+      slope = ((y2 - y1) / float(x2 - x1))
+      slopes.append(slope)
+      print 'x1:', x1, 'x2:', x2, 'y1:', y1, 'y2:', y2
+  slope_avg = np.sum(slopes) / len(slopes)
+  print 'average slope:', slope_avg
+  angle = np.arctan(slope_avg) * 180/np.pi
+  # correct for change in coordinate system
+  if (angle < 0):
+    angle = angle * -1
+  else:
+    angle = 180 - angle
+  print 'angle:', angle
+
+# convert a slope and intercept into a tuple of start and end points
+def slope_to_points(line, y1=0, y2=HEIGHT):
+  if line is None:
+    return 0, 0, 1, 1
+  #else
+  a, b = line
+  x1 = int((y1 - b) / a)
+  x2 = int((y2 - b) / a)
+  y1 = int(y1)
+  y2 = int(y2)
+  return x1, y1, x2, y2
+
+# find the average line from a set of lines
+def avg_line(lines):
+  avg = avg_slop_intercept(lines)
+  return slope_to_points(avg)
 
 def birds_eye_view(img):
   #points in camera view, corresponding to empirical observation
@@ -129,10 +156,12 @@ def main():
 
     if DEBUG:
       # draw Hough lines on bird's eye view image
-      img_lines = draw_lines(img_erode, lines)
+      img_lines = draw_lines(img_birds, lines, 0, HEIGHT/2)
+      average_angle(lines)
       # show the frame
       #cv2.imshow("Image", image)
-      cv2.imshow("Isolated", img_erode)
+      #cv2.imshow("Isolated", img_erode)
+      cv2.imshow("Birds Eye", img_birds)
       cv2.waitKey(1)
   
     # clear the stream in preparation for the next frame
